dcb617f9a7fc2fbf1e3cc2c62b41b6e2592d59a9
Hanndroid <hannah.m.evans4@gmail.com>
2020-07-07 15:58:46 +0100

    add styling to store section

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log("I am embedded into the HTML file!");
+
+var john = ["John", "Smith", 1990, "teacher", false];
+
+john.push("blue"); // adds item to end of array
+john.unshift("Mr."); // add item to start of array
+john.pop(); // removes last item in the array
+
+console.log(john);
