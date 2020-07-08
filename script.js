d531416df1485c697091f63e7c836ffd3f56f502
Hanndroid <hannah.m.evans4@gmail.com>
2020-07-08 12:27:09 +0100

    'remove' and cart total update when selected

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-console.log("I am embedded into the HTML file!");
+/*  console.log("I am embedded into the HTML file!");
 
 var john = ["John", "Smith", 1990, "teacher", false];
 
@@ -7,3 +7,44 @@ john.unshift("Mr."); // add item to start of array
 john.pop(); // removes last item in the array
 
 console.log(john);
+*/
+
+if (document.readyState == "loading") {
+  document.addEventListener("DOMContentLoaded", ready);
+} else {
+  ready();
+}
+
+function ready() {
+  var removeCartItemButtons = document.getElementsByClassName("btn-danger");
+  console.log(removeCartItemButtons);
+  for (var i = 0; i < removeCartItemButtons.length; i++) {
+    var button = removeCartItemButtons[i];
+    button.addEventListener("click", removeCartItem);
+  }
+}
+
+function removeCartItem(event) {
+  var buttonClicked = event.target;
+  buttonClicked.parentElement.parentElement.remove();
+  updateCartTotal();
+}
+
+function updateCartTotal() {
+  var cartItemContainer = document.getElementsByClassName("cart-items")[0];
+  var cartRows = cartItemContainer.getElementsByClassName("cart-row");
+  var total = 0;
+  for (var i = 0; i < cartRows.length; i++) {
+    var cartRow = cartRows[i];
+    var priceElement = cartRow.getElementsByClassName("cart-price")[0];
+    var quantityElement = cartRow.getElementsByClassName(
+      "cart-quantity-input"
+    )[0];
+    //console.log(priceElement, quantityElement);
+    var price = parseFloat(priceElement.innerText.replace("$", " "));
+    var quantity = quantityElement.value;
+    total = total + price * quantity;
+  }
+  document.getElementsByClassName("cart-total-price")[0].innerText =
+    "$" + total;
+}
